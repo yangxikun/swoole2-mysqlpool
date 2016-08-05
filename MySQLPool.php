b2564d5f4696082b54c28cf65c287e6f0eade2e6
roketyyang@tencent.com <roketyyang@localhost.localdomain>
2016-08-05 20:58:31 +0800

    init

---
+++ MySQLPool.php
@@ -0,0 +1,119 @@
+<?php
+namespace Swoole\Coroutine\Pool;
+
+class MySQLException extends \Exception {}
+
+class MySQLPool
+{
+	static protected $init = false;
+
+	static protected $spareConns = [];
+
+	static protected $busyConns = [];
+
+	static protected $connsConfig;
+
+	static protected $connsNameMap = [];
+
+	static protected $pendingFetchCount = [];
+
+	static protected $resumeFetchCount = [];
+
+	static public function init(array $connsConfig)
+	{
+		if (self::$init) {
+			return;
+		}
+		self::$connsConfig = $connsConfig;
+		foreach ($connsConfig as $name => $config) {
+			self::$spareConns[$name] = [];
+			self::$busyConns[$name] = [];
+			self::$pendingFetchCount[$name] = 0;
+			self::$resumeFetchCount[$name] = 0;
+			if ($config['maxSpareConns'] <= 0 || $config['maxConns'] <= 0) {
+				throw new MySQLException("Invalid maxSpareConns or maxConns in {$name}");
+			}
+		}
+		self::$init = true;
+	}
+
+	static public function recycle(\Swoole\Coroutine\MySQL $conn)
+	{
+		if (!self::$init) {
+			throw new MySQLException('Should call MySQLPool::init.');
+		}
+
+		$id = spl_object_hash($conn);
+		$connName = self::$connsNameMap[$id];
+		if (isset(self::$busyConns[$connName][$id])) {
+			unset(self::$busyConns[$connName][$id]);
+		} else {
+			throw new MySQLException('Unknow MySQL connection.');
+		}
+
+		$connsPool = &self::$spareConns[$connName];
+		if ($conn->connected) {
+			if (count($connsPool) >= self::$connsConfig[$connName]['maxSpareConns']) {
+				$conn->close();
+			} else {
+				$connsPool[] = $conn;
+				if (self::$pendingFetchCount[$connName] > 0) {
+					self::$resumeFetchCount[$connName]++;
+					self::$pendingFetchCount[$connName]--;
+					\Swoole\Coroutine::resume('MySQLPool::' . $connName);
+				}
+				return;
+			}
+		}
+
+		unset(self::$connsNameMap[$id]);
+	}
+
+	static public function fetch($connName)
+	{
+		if (!self::$init) {
+			throw new MySQLException('Should call MySQLPool::init!');
+		}
+
+		if (!isset(self::$connsConfig[$connName])) {
+			throw new MySQLException("Unvalid connName: {$connName}.");
+		}
+
+		$connsPool = &self::$spareConns[$connName];
+		if (!empty($connsPool) && count($connsPool) > self::$resumeFetchCount[$connName]) {
+			$conn = array_pop($connsPool);
+			self::$busyConns[$connName][spl_object_hash($conn)] = $conn;
+
+			return $conn;
+		}
+
+		if (count(self::$busyConns[$connName]) + count($connsPool) == self::$connsConfig[$connName]['maxConns']) {
+			self::$pendingFetchCount[$connName]++;
+			if (\Swoole\Coroutine::suspend('MySQLPool::' . $connName) == false) {
+				self::$pendingFetchCount[$connName]--;
+				throw new MySQLException('Reach max connections! Cann\'t pending fetch!');
+			}
+			self::$resumeFetchCount[$connName]--;
+			if (!empty($connsPool)) {
+				$conn = array_pop($connsPool);
+				self::$busyConns[$connName][spl_object_hash($conn)] = $conn;
+
+				return $conn;
+			} else {
+				return false;//should not happen
+			}
+		}
+
+		$conn = new \Swoole\Coroutine\MySQL();
+		$id = spl_object_hash($conn);
+		self::$connsNameMap[$id] = $connName;
+		self::$busyConns[$connName][$id] = $conn;
+		if ($conn->connect(self::$connsConfig[$connName]['serverInfo']) == false) {
+			unset(self::$busyConns[$connName][$id]);
+			unset(self::$connsNameMap[$id]);
+			throw new MySQLException('Cann\'t connect to MySQL server: ' . json_encode(self::$connsConfig[$connName]['serverInfo']));
+		}
+
+		return $conn;
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,100 @@
-# swoole2-mysqlpool
-swoole 2 mysql connection pool
+基于Swoole2协程特性实现的MySQL连接池
+- - -
+使用Swoole2的\Swoole\Coroutine\MySQL创建连接，通过静态类和静态成员属性维护连接池，不同协程可以共享该连接池。
+
+排队机制（先进先出）使用协程的特殊功能实现：
+
+* `\Swoole\Coroutine::resume($name)`：从$name队列中恢复一个挂起的协程执行；
+* `\Swoole\Coroutine::suspend($name)`：将当前协程挂起到$name队列上。
+
+### 限制
+
+* 每个worker都有各自的MySQL连接池，且不同worker之间无法共享彼此的MySQL连接池；
+* 可能存在各个worker进程间连接池利用率不同（依赖业务实现）。
+
+### 优点
+
+* 与独立的连接池（worker进程间可共享的连接池实现）对比，无进程间通信开销；
+* 独立的连接池需要增加运维成本。
+
+### 使用方法
+
+```php
+/**
+ * 初始化连接池
+ *
+ * @param array $connsConfig 配置数组
+ *	 [
+ *		 'connName1' => [
+ *			'serverInfo' => ['host' => '127.0.0.1', 'user' => 'test', 'password' => 'pass', 'database' => 'tt', 'charset' => 'utf8'], //\Swoole\Coroutine\MySQL的connect参数
+ *			'maxSpareConns' => 5, //最大空闲连接数
+ *			'maxConns' => 10, //最大连接数
+ *		 ],
+ *		 'connName2' => [
+ *			'serverInfo' => ['host' => '127.0.0.2', 'user' => 'test', 'password' => 'pass', 'database' => 'tt', 'charset' => 'utf8'], //\Swoole\Coroutine\MySQL的connect参数
+ *			'maxSpareConns' => 5, //最大空闲连接数
+ *			'maxConns' => 10, //最大连接数
+ *		 ],
+ *	 ]
+ */
+Swoole\Coroutine\Pool\MySQLPool::init(array $connsConfig)
+
+/**
+ * 回收连接，该连接必须是从连接池中获取的连接
+ *
+ * @param \Swoole\Coroutine\MySQL $conn 从连接池中获取的连接
+ */
+Swoole\Coroutine\Pool\MySQLPool::recycle(\Swoole\Coroutine\MySQL $conn)
+
+/**
+ * 从连接池中获取一条连接
+ *
+ * @param string $connName init时配置的连接，根据连接名称获取对应的连接
+ * @return \Swoole\Coroutine\MySQL 返回一个连接实例
+ */
+Swoole\Coroutine\Pool\MySQLPool::fetch($connName)
+```
+
+### 使用示例
+
+```php
+<?php
+require 'MySQLPool.php';
+use Swoole\Coroutine\Pool\MySQLPool;
+
+$server = new Swoole\Http\Server("127.0.0.1", 9502, SWOOLE_BASE);
+$server->set([
+        'worker_num' => 1,
+]);
+$server->on('Request', function($request, $response) {
+        MySQLPool::init([
+                'test' => [
+                        'serverInfo' => ['host' => '192.168.244.128', 'user' => 'mha_manager', 'password' => 'mhapass', 'database' => 'tt', 'charset' => 'utf8'],
+                        'maxSpareConns' => 5,
+                        'maxConns' => 10
+                ],
+        ]);
+        $swoole_mysql = MySQLPool::fetch('test');
+        $ret = $swoole_mysql->query('select sleep(1)');
+        MySQLPool::recycle($swoole_mysql);
+        $response->end('Test End');
+});
+$server->start();
+```
+
+压测命令：`ab -c 20 -n 100 -s 100 http://127.0.0.1:9502/`，20并发，共100个请求。
+
+压测结果：
+
+![ab](img/ab.png)
+
+压测时连接情况：
+
+![ab](img/ab.png)
+
+压测后连接情况：
+
+
+这里服务端只有1个worker进程在工作，完成100个请求，且每个请求的SQL查询是sleep 1秒，花了约11秒，如果是php-fpm+mysqli这样的模式，1个worker进程，那么得花100秒。
+
+所以这正是协程的优势所在，利用非阻塞IO+协程切换，1个worker进程能同时处理多个客户端请求，大大提高了并发量。
